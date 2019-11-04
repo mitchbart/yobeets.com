d164b31e1448c1197b1587ba268e0590d23b45e5
yobeets <mitchell.bartolo@gmail.com>
2019-11-04 16:17:25 +1000

    just working through fundamentals

---
+++ js/fundamentals.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log("why hello there!");
+console.log("stuff also be deleted in the console, not a lot to see here")
+
+
+//fundamentals part 1
+
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+console.log(admin);
