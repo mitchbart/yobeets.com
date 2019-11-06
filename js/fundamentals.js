b558a58bde68a13f2352d68ce9cc82771c8924dc
mitchell <mitchell.bartolo@gmail.com>
2019-11-06 12:56:24 +1000

    testing on manjaro

--- js/fundamentals.js
+++ js/fundamentals.js
@@ -6,10 +6,13 @@ console.log("stuff also be deleted in the console, not a lot to see here")
 
 //fundamentals part 1
 
-let admin;
-let name;
+//let admin;
+//let name;
 
-name = "John";
-admin = name;
+//name = "John";
+//admin = name;
 
-console.log(admin);
+//console.log(admin);
+
+//fundamentals part 2
+
